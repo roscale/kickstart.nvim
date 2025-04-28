38f17198dad3fd6cfbd4dfe883ce1e51396e21fe
Alex Rosca <roscaalex19@gmail.com>
2025-04-28 23:40:27 +0200

    Comment with gc

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -101,4 +101,20 @@ return {
     'Pocco81/auto-save.nvim',
     opts = {},
   },
+
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      mappings = {
+        extra = false,
+      },
+      toggler = {
+        line = 'gc',
+      },
+    },
+    config = function(_, opts)
+      vim.keymap.del('n', 'gcc')
+      require('Comment').setup(opts)
+    end,
+  },
 }
