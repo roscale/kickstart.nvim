2c6e13316879d8d88a3e866febbaa5d9e85d167f
Alex Rosca <roscaalex19@gmail.com>
2025-04-29 23:44:56 +0200

    Multicursors

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -117,4 +117,56 @@ return {
       require('Comment').setup(opts)
     end,
   },
+
+  {
+    'jake-stewart/multicursor.nvim',
+    branch = '1.0',
+    config = function()
+      local mc = require 'multicursor-nvim'
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add cursor above/below the main cursor.
+      set({ 'n', 'x' }, '<C-Up>', function()
+        mc.lineAddCursor(-1)
+      end)
+      set({ 'n', 'x' }, '<C-Down>', function()
+        mc.lineAddCursor(1)
+      end)
+
+      -- Add a new cursor by matching word/selection
+      set({ 'n', 'x' }, '<C-n>', function()
+        mc.matchAddCursor(1)
+      end)
+      set({ 'n', 'x' }, '<C-S-n>', function()
+        mc.matchAddCursor(-1)
+      end)
+
+      -- Add and remove cursors with control + left click.
+      set('n', '<C-leftmouse>', mc.handleMouse)
+      set('n', '<C-leftdrag>', mc.handleMouseDrag)
+      set('n', '<C-leftrelease>', mc.handleMouseRelease)
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- Select a different cursor as the main one.
+        layerSet({ 'n', 'x' }, '<C-Left>', mc.prevCursor)
+        layerSet({ 'n', 'x' }, '<C-Right>', mc.nextCursor)
+
+        -- Clear cursors using escape.
+        layerSet('n', '<esc>', function()
+          mc.clearCursors()
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, 'MultiCursorCursor', { reverse = true })
+      hl(0, 'MultiCursorVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+      hl(0, 'MultiCursorMatchPreview', { link = 'Search' })
+    end,
+  },
 }
