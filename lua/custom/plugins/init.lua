2760dbaceb83d23f6f44b7bc4dfc6496d5a0600f
Alex Rosca <roscaalex19@gmail.com>
2025-05-25 00:40:29 +0200

    Remove diagnostics from bufferline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -164,14 +164,7 @@ return {
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {
-      options = {
-        diagnostics = 'nvim_lsp',
-        diagnostics_indicator = function(_, _, diag, _)
-          return (diag.error and diag.error .. ' ' or '') .. (diag.warning and diag.warning .. ' ' or '') .. (diag.hint and diag.hint .. '' or '')
-        end,
-      },
-    },
+    opts = {},
   },
 
   {
