2e43fa32a24ea1fe654ce7fb63c64a6f47040afb
Alex Rosca <roscaalex19@gmail.com>
2025-05-29 22:43:43 +0200

    Change keymap for treesitter function call

--- init.lua
+++ init.lua
@@ -449,7 +449,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>st', builtin.builtin, { desc = '[S]earch Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
@@ -953,9 +953,9 @@ require('lazy').setup({
           },
           a = ai.gen_spec.treesitter { a = '@parameter.outer', i = '@parameter.inner' },
           p = ai.gen_spec.treesitter { a = '@parameter.outer', i = '@parameter.inner' },
-          f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+          f = ai.gen_spec.treesitter { a = '@call.outer', i = '@call.inner' },
+          F = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
           c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' },
-          u = ai.gen_spec.function_call(),
         },
       }
 
