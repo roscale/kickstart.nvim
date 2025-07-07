83029d4a2e60b1f871ee82259d3df01ca6b0c65f
Alex Rosca <roscaalex19@gmail.com>
2025-07-07 23:22:31 +0200

    Remove unused `o` text object

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,10 +6,6 @@ return {
     ai.setup {
       n_lines = 500,
       custom_textobjects = {
-        o = ai.gen_spec.treesitter {
-          a = { '@block.outer', '@conditional.outer', '@loop.outer' },
-          i = { '@block.inner', '@conditional.inner', '@loop.inner' },
-        },
         a = ai.gen_spec.treesitter { a = '@parameter.outer', i = '@parameter.inner' },
         p = ai.gen_spec.treesitter { a = '@parameter.outer', i = '@parameter.inner' },
         f = ai.gen_spec.treesitter { a = '@call.outer', i = '@call.inner' },
