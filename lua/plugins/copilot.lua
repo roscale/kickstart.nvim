51625c1a7a466a2f2bce3ae93e71ac7e71a1ea90
Alex Rosca <roscaalex19@gmail.com>
2025-08-03 23:59:27 +0200

    Disable copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
-  'zbirenbaum/copilot.lua',
-  event = 'InsertEnter',
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-  },
-  config = function(_, opts)
-    require('copilot').setup(opts)
-    vim.cmd [[highlight CopilotSuggestion guifg=#888888]]
-  end,
+  -- 'zbirenbaum/copilot.lua',
+  -- event = 'InsertEnter',
+  -- opts = {
+  --   suggestion = { enabled = false },
+  --   panel = { enabled = false },
+  -- },
+  -- config = function(_, opts)
+  --   require('copilot').setup(opts)
+  --   vim.cmd [[highlight CopilotSuggestion guifg=#888888]]
+  -- end,
 }
