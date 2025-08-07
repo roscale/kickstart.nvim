a03064aca61d3cea5872e4e44662c69715695adb
Alex Rosca <roscaalex19@gmail.com>
2025-08-07 23:38:14 +0200

    Scroll with Shift+Up/Down

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -3,4 +3,16 @@ return {
   opts = {
     easing = 'quadratic',
   },
+  config = function(_, opts)
+    local neoscroll = require 'neoscroll'
+    neoscroll.setup(opts)
+
+    vim.keymap.set({ 'n', 'v', 'x' }, '<S-Up>', function()
+      neoscroll.ctrl_u { duration = 250 }
+    end, { desc = 'Half-page up' })
+
+    vim.keymap.set({ 'n', 'v', 'x' }, '<S-Down>', function()
+      neoscroll.ctrl_d { duration = 250 }
+    end, { desc = 'Half-page down' })
+  end,
 }
